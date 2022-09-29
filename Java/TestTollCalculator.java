86db69bc0e38851fff4e69dea9a0c0633b4e3c85
poreddymadhukar <114689162+poreddymadhukar@users.noreply.github.com>
2022-09-30 00:13:32 +0530

    Code changes committed by Madhukar Poreddy

---
+++ Java/TestTollCalculator.java
@@ -0,0 +1,282 @@
+package Java;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;  
+  
+public class TestTollCalculator {  
+	
+	@BeforeClass  
+    public static void setUpBeforeClass() throws Exception {  
+        System.out.println("before class");  
+    }   
+    
+	/**
+	 * Only one entry during normal traffic hours.
+	 * 
+	 */
+	
+	@Test  
+    public void testOnlyOneEntryInNoramTraffic(){
+    	Vehicle vehicle = new Car();
+    	ArrayList<Date> dateList = new ArrayList<Date>();
+		
+    	Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 06);
+		cal.set(Calendar.MINUTE, 01);
+		cal.set(Calendar.SECOND, 01);
+		dateList.add(cal.getTime());
+		System.out.println(vehicle.getType() + " Entry 1 at " + cal.getTime());
+		
+		TollCalculator tollCalculatorObj = new TollCalculator(); 
+		Date[] dateArray = new Date[dateList.size()]; 
+		
+		 
+		int tollFee = tollCalculatorObj.getTollFee(vehicle, dateList.toArray(dateArray));
+        
+		System.out.println(tollFee);
+    }
+	
+	/**
+	 * One Entry during rush hour
+	 */
+	@Test  
+    public void testOnlyOneEntryDuringRushHour(){
+		
+		/* Rush Hour Timings are
+		 * 
+		 *  7 to 7.59 
+		 *  
+		 *  15.30 to 16.59
+		 *   
+		 */
+		int expectedTollFeeDuringRushHour = 18;
+		
+		Vehicle vehicle = new Car();
+		
+    	ArrayList<Date> dateList = new ArrayList<Date>();
+		
+    	Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 35);
+		cal.set(Calendar.SECOND, 10);
+		dateList.add(cal.getTime());
+		System.out.println(vehicle.getType() + " Entry 1 During Rush Hour at [ " + cal.getTime() +" ]");
+		
+		TollCalculator tollCalculatorObj = new TollCalculator(); 
+		Date[] dateArray = new Date[dateList.size()]; 
+		
+		 
+		int tollFee = tollCalculatorObj.getTollFee(vehicle, dateList.toArray(dateArray));
+        
+		Assert.assertEquals(expectedTollFeeDuringRushHour,tollFee);  
+		
+		System.out.println(tollFee);
+    }
+	
+	/**
+	 * This test case will test for same vehicle,
+	 * which is passed multiple times during rush hour and within 1hr.
+	 * 
+	 */
+	@Test  
+    public void testMultipleEntriesDuringRushHour(){
+		
+		/* Rush Hour Timings are
+		 * 
+		 *  7 to 7.59 
+		 *  
+		 *  15.30 to 16.59
+		 *   
+		 */
+		int expectedTollFeeDuringRushHour = 18;
+		
+		Vehicle vehicle = new Car();
+		
+    	ArrayList<Date> dateList = new ArrayList<Date>();
+		
+    	Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 5);
+		cal.set(Calendar.SECOND, 10);
+		dateList.add(cal.getTime());
+		System.out.println(vehicle.getType() + " Entry 1 During Rush Hour at [ " + cal.getTime() +" ]");
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 25);
+		cal.set(Calendar.SECOND, 12);
+		dateList.add(cal.getTime());
+		System.out.println(vehicle.getType() + " Entry 2 During Rush Hour at [ " + cal.getTime() +" ]");
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 55);
+		cal.set(Calendar.SECOND, 12);
+		dateList.add(cal.getTime());
+		System.out.println(vehicle.getType() + " Entry 3 During Rush Hour at [ " + cal.getTime() +" ]");
+		
+		
+		TollCalculator tollCalculatorObj = new TollCalculator(); 
+		Date[] dateArray = new Date[dateList.size()]; 
+		
+		 
+		int tollFee = tollCalculatorObj.getTollFee(vehicle, dateList.toArray(dateArray));
+        
+		Assert.assertEquals(expectedTollFeeDuringRushHour,tollFee);  
+		
+		System.out.println(tollFee);
+    }
+    
+    /**
+	 * This test case will test for same vehicle,
+	 * which is passed two times during two different rush hours.
+	 * 
+	 */
+	@Test  
+    public void testMultipleEntriesDuringDifferentRushHours(){
+		
+		/* Rush Hour Timings are
+		 * 
+		 *  7 to 7.59 
+		 *  
+		 *  15.30 to 16.59
+		 *   
+		 */
+		int expectedTollFeeDuringRushHour = 36;
+		
+		Vehicle vehicle = new Car();
+		
+    	ArrayList<Date> dateList = new ArrayList<Date>();
+		
+    	Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 5);
+		cal.set(Calendar.SECOND, 10);
+		dateList.add(cal.getTime());
+		System.out.println(vehicle.getType() + " Entry 1 During Rush Hour at [ " + cal.getTime() +" ]");
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 15);
+		cal.set(Calendar.MINUTE, 45);
+		cal.set(Calendar.SECOND, 12);
+		dateList.add(cal.getTime());
+		System.out.println(vehicle.getType() + " Entry 2 During Rush Hour at [ " + cal.getTime() +" ]");
+		 
+		
+		TollCalculator tollCalculatorObj = new TollCalculator(); 
+		Date[] dateArray = new Date[dateList.size()]; 
+		
+		 
+		int tollFee = tollCalculatorObj.getTollFee(vehicle, dateList.toArray(dateArray));
+        
+		Assert.assertEquals(expectedTollFeeDuringRushHour,tollFee);  
+		
+		System.out.println(tollFee);
+    }
+    
+    /**
+	 * This test case will test for same vehicle,
+	 * 
+	 * which is passed multiple times within same day.
+	 * 
+	 */
+	@Test  
+    public void testMaximumDayTollFee(){
+		
+		/* Rush Hour Timings are
+		 * 
+		 *  7 to 7.59 
+		 *  
+		 *  15.30 to 16.59
+		 *   
+		 */
+		int maxDayTollFee = 60;
+		
+		Vehicle vehicle = new Car();
+		TollCalculator tollCalculatorObj = new TollCalculator();
+		 
+		ArrayList<Date> dateList = prepareInput(vehicle);
+		Date[] dateArray = new Date[dateList.size()];
+		
+		 
+		int tollFee = tollCalculatorObj.getTollFee(vehicle, dateList.toArray(dateArray));
+		
+		System.out.println(tollFee);	
+		
+		Assert.assertEquals(maxDayTollFee,tollFee);
+    } 
+    
+    /**
+     * This method will prepare the test data
+     * @param v
+     * @return
+     */
+    private ArrayList<Date> prepareInput(Vehicle v) {
+		ArrayList<Date> dateList = new ArrayList<Date>();
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 6);
+		cal.set(Calendar.MINUTE, 01);
+		cal.set(Calendar.SECOND, 01);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 1>" + cal.getTime());
+
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 25);
+		cal.set(Calendar.SECOND, 59);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 2>" + cal.getTime());
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 45);
+		cal.set(Calendar.SECOND, 50);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 3>" + cal.getTime());
+		
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 9);
+		cal.set(Calendar.MINUTE, 12);
+		cal.set(Calendar.SECOND, 59);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 4>" + cal.getTime());
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 12);
+		cal.set(Calendar.MINUTE, 12);
+		cal.set(Calendar.SECOND, 59);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 5>" + cal.getTime());
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 15);
+		cal.set(Calendar.MINUTE, 12);
+		cal.set(Calendar.SECOND, 59);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 6>" + cal.getTime());
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 20);
+		cal.set(Calendar.MINUTE, 12);
+		cal.set(Calendar.SECOND, 59);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 7>" + cal.getTime());
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 22);
+		cal.set(Calendar.MINUTE, 12);
+		cal.set(Calendar.SECOND, 59);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 8>" + cal.getTime());	
+		
+		
+		return dateList;
+
+	}
+}  
--- Java/TollCalculator.java
+++ Java/TollCalculator.java
@@ -1,110 +1,196 @@
+package Java;
 
 import java.util.*;
 import java.util.concurrent.*;
 
 public class TollCalculator {
+	
 
-  /**
-   * Calculate the total toll fee for one day
-   *
-   * @param vehicle - the vehicle
-   * @param dates   - date and time of all passes on one day
-   * @return - the total toll fee for that day
-   */
-  public int getTollFee(Vehicle vehicle, Date... dates) {
-    Date intervalStart = dates[0];
-    int totalFee = 0;
-    for (Date date : dates) {
-      int nextFee = getTollFee(date, vehicle);
-      int tempFee = getTollFee(intervalStart, vehicle);
+	/**
+	 * Calculate the total toll fee for one day
+	 *
+	 * @param vehicle
+	 *            - the vehicle
+	 * @param dates
+	 *            - date and time of all passes on one day
+	 * @return - the total toll fee for that day
+	 */
+	public int getTollFee(Vehicle vehicle, Date... dates) {
+		Date intervalStart = dates[0];
+		int totalFee = 0;
+		int tmpTripFee = 0;
+		for (Date date : dates) {  
 
-      TimeUnit timeUnit = TimeUnit.MINUTES;
-      long diffInMillies = date.getTime() - intervalStart.getTime();
-      long minutes = timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS);
+			TimeUnit timeUnit = TimeUnit.MINUTES;
+			long diffInMillies = date.getTime() - intervalStart.getTime();
+			long minutes = timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS);
 
-      if (minutes <= 60) {
-        if (totalFee > 0) totalFee -= tempFee;
-        if (nextFee >= tempFee) tempFee = nextFee;
-        totalFee += tempFee;
-      } else {
-        totalFee += nextFee;
-      }
-    }
-    if (totalFee > 60) totalFee = 60;
-    return totalFee;
-  }
+			if (minutes > 60) {
+				intervalStart = date;
+				diffInMillies = date.getTime() - intervalStart.getTime();
+				minutes = timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS); 
+				tmpTripFee = 0;
+			}
+			
+			int endFee = getTollFee(date, vehicle);
+			int startFee = getTollFee(intervalStart, vehicle);
+			
+			// System.out.println(totalFee +" entry > "+date+" minutes
+			// "+minutes);
+			// System.out.println("startFee "+startFee);
+			// System.out.println("endFee "+endFee);
 
-  private boolean isTollFreeVehicle(Vehicle vehicle) {
-    if(vehicle == null) return false;
-    String vehicleType = vehicle.getType();
-    return vehicleType.equals(TollFreeVehicles.MOTORBIKE.getType()) ||
-           vehicleType.equals(TollFreeVehicles.TRACTOR.getType()) ||
-           vehicleType.equals(TollFreeVehicles.EMERGENCY.getType()) ||
-           vehicleType.equals(TollFreeVehicles.DIPLOMAT.getType()) ||
-           vehicleType.equals(TollFreeVehicles.FOREIGN.getType()) ||
-           vehicleType.equals(TollFreeVehicles.MILITARY.getType());
-  }
+			if (minutes <= 60) {
+				if (endFee > startFee) {					
+					tmpTripFee = endFee;
+					totalFee = endFee;
+				} else if (startFee == endFee && tmpTripFee != 0) {
+					tmpTripFee = endFee;
+				} else {
+					totalFee += Math.max(startFee, endFee);
+					tmpTripFee = endFee;
+				}
+			} 
+		}
+		if (totalFee > 60) {
+			totalFee = 60;
+		}
+		return totalFee;
+	}
 
-  public int getTollFee(final Date date, Vehicle vehicle) {
-    if(isTollFreeDate(date) || isTollFreeVehicle(vehicle)) return 0;
-    Calendar calendar = GregorianCalendar.getInstance();
-    calendar.setTime(date);
-    int hour = calendar.get(Calendar.HOUR_OF_DAY);
-    int minute = calendar.get(Calendar.MINUTE);
+	private boolean isTollFreeVehicle(Vehicle vehicle) {
+		if (vehicle == null)
+			return false;
+		String vehicleType = vehicle.getType();
+		return vehicleType.equals(TollFreeVehicles.MOTORBIKE.getType())
+				|| vehicleType.equals(TollFreeVehicles.TRACTOR.getType())
+				|| vehicleType.equals(TollFreeVehicles.EMERGENCY.getType())
+				|| vehicleType.equals(TollFreeVehicles.DIPLOMAT.getType())
+				|| vehicleType.equals(TollFreeVehicles.FOREIGN.getType())
+				|| vehicleType.equals(TollFreeVehicles.MILITARY.getType());
+	}
 
-    if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-    else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-    else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-    else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-    else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-    else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-    else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-    else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-    else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-    else return 0;
-  }
+	public int getTollFee(final Date date, Vehicle vehicle) {
+		if (isTollFreeDate(date) || isTollFreeVehicle(vehicle))
+			return 0;
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.setTime(date);
+		int hour = calendar.get(Calendar.HOUR_OF_DAY);
+		int minute = calendar.get(Calendar.MINUTE);
 
-  private Boolean isTollFreeDate(Date date) {
-    Calendar calendar = GregorianCalendar.getInstance();
-    calendar.setTime(date);
-    int year = calendar.get(Calendar.YEAR);
-    int month = calendar.get(Calendar.MONTH);
-    int day = calendar.get(Calendar.DAY_OF_MONTH);
+		if (hour == 6 && minute >= 0 && minute <= 29)
+			return 8;
+		else if (hour == 6 && minute >= 30 && minute <= 59)
+			return 13;
+		else if (hour == 7 && minute >= 0 && minute <= 59)
+			return 18;
+		else if (hour == 8 && minute >= 0 && minute <= 29)
+			return 13;
+		else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59)
+			return 8;
+		else if (hour == 15 && minute >= 0 && minute <= 29)
+			return 13;
+		else if (hour == 15 && minute >= 30 || hour == 16 && minute <= 59)
+			return 18;
+		else if (hour == 17 && minute >= 0 && minute <= 59)
+			return 13;
+		else if (hour == 18 && minute >= 0 && minute <= 29)
+			return 8;
+		else if ((hour == 18 && minute >= 30) || (hour <= 23 && minute <= 59))
+			return 8;
+		else if ((hour >= 00 && hour <= 5) && minute <= 59)
+			return 8;
+		else
+			return 0;
+	}
 
-    int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-    if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) return true;
+	private Boolean isTollFreeDate(Date date) {
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.setTime(date);
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-    if (year == 2013) {
-      if (month == Calendar.JANUARY && day == 1 ||
-          month == Calendar.MARCH && (day == 28 || day == 29) ||
-          month == Calendar.APRIL && (day == 1 || day == 30) ||
-          month == Calendar.MAY && (day == 1 || day == 8 || day == 9) ||
-          month == Calendar.JUNE && (day == 5 || day == 6 || day == 21) ||
-          month == Calendar.JULY ||
-          month == Calendar.NOVEMBER && day == 1 ||
-          month == Calendar.DECEMBER && (day == 24 || day == 25 || day == 26 || day == 31)) {
-        return true;
-      }
-    }
-    return false;
-  }
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+		if (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY)
+			return true;
 
-  private enum TollFreeVehicles {
-    MOTORBIKE("Motorbike"),
-    TRACTOR("Tractor"),
-    EMERGENCY("Emergency"),
-    DIPLOMAT("Diplomat"),
-    FOREIGN("Foreign"),
-    MILITARY("Military");
-    private final String type;
+		if (year == 2022) {
+			if (month == Calendar.JANUARY && day == 1 || month == Calendar.MARCH && (day == 28 || day == 29)
+					|| month == Calendar.APRIL && (day == 1 || day == 30)
+					|| month == Calendar.MAY && (day == 1 || day == 8 || day == 9)
+					|| month == Calendar.JUNE && (day == 5 || day == 6 || day == 21) || month == Calendar.JULY
+					|| month == Calendar.NOVEMBER && day == 1
+					|| month == Calendar.DECEMBER && (day == 24 || day == 25 || day == 26 || day == 31)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    TollFreeVehicles(String type) {
-      this.type = type;
-    }
+	private enum TollFreeVehicles {
+		MOTORBIKE("Motorbike"), TRACTOR("Tractor"), EMERGENCY("Emergency"), DIPLOMAT("Diplomat"), FOREIGN(
+				"Foreign"), MILITARY("Military");
+		private final String type;
 
-    public String getType() {
-      return type;
-    }
-  }
+		TollFreeVehicles(String type) {
+			this.type = type;
+		}
+
+		public String getType() {
+			return type;
+		}
+	}
+	
+	
+	public static void main(String[] ar) {
+		Vehicle v = new Car();
+		TollCalculator tollCalculatorObj = new TollCalculator();
+		 
+		ArrayList<Date> dateList = tollCalculatorObj.prepareInput(v);
+		Date[] dateArray = new Date[dateList.size()];
+		
+		//get toll fee
+		int tollFee = tollCalculatorObj.getTollFee(v, dateList.toArray(dateArray));
+		System.out.println("\n");
+		System.out.format("Total Toll Fee %d SEK for %s ", tollFee, v.getType());
+
+	}
+
+	private ArrayList<Date> prepareInput(Vehicle v) {
+		ArrayList<Date> dateList = new ArrayList<Date>();
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 06);
+		cal.set(Calendar.MINUTE, 01);
+		cal.set(Calendar.SECOND, 01);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 1>" + cal.getTime());
+
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 25);
+		cal.set(Calendar.SECOND, 59);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 2>" + cal.getTime());
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 45);
+		cal.set(Calendar.SECOND, 50);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 3>" + cal.getTime());
+		
+		
+		cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 9);
+		cal.set(Calendar.MINUTE, 12);
+		cal.set(Calendar.SECOND, 59);
+		dateList.add(cal.getTime());
+		System.out.println(v.getType() + " Entry 4>" + cal.getTime());
+		
+		
+		return dateList;
+
+	}
 }
-
